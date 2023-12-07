74b2af924885da5e37f5c516ca06008cde2a7726
reinhartlim <18221154@std.stei.itb.ac.id>
2023-12-07 09:34:10 +0700

    fix auth with email

--- app/auth/oauth2.py
+++ app/auth/oauth2.py
@@ -25,10 +25,10 @@ def create_access_token(data: dict):
 def verify_access_token(token: str, credentials_exception):
     try:
         payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
-        id = payload.get("user_id")
-        if id is None:
+        email = payload.get("user_email")
+        if email is None:
             raise credentials_exception
-        token_data = schemas.TokenData(id=id)
+        token_data = schemas.TokenData(email=email)
     except JWTError:
         raise credentials_exception
     return token_data
@@ -39,6 +39,18 @@ async def get_current_user(token: str = Depends(oauth2_scheme), db: Session = De
         detail="Could not validate credentials", 
         headers={"WWW-Authenticate": "Bearer"}
     )
+    token_data = token
+    # print(token_data)
     token = verify_access_token(token, credentials_exception)
-    user = db.query(models.User).filter(models.User.id == token.id).first()
+    user = db.query(models.User).filter(models.User.email == token.email).first()
+    # print(user)
     return user
+
+async def get_current_token(token: str = Depends(oauth2_scheme)):
+    credentials_exception = HTTPException(
+        status_code=status.HTTP_401_UNAUTHORIZED, 
+        detail="Could not validate credentials", 
+        headers={"WWW-Authenticate": "Bearer"}
+    )
+    token_data = token
+    return token_data
--- app/routers/auth.py
+++ app/routers/auth.py
@@ -22,5 +22,5 @@ async def login(user_credentials: OAuth2PasswordRequestForm = Depends(), db: Ses
         raise HTTPException(
             status_code=status.HTTP_403_FORBIDDEN, detail=f"Invalid Credentials")
     
-    access_token = oauth2.create_access_token(data={"user_id": user.id})
+    access_token = oauth2.create_access_token(data={"user_email": user.email})
     return {"access_token": access_token, "token_type": "bearer"}
