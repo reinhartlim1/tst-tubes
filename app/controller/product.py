796cf19e5051483a3593293e1bd838e46795716c
reinhartlim <18221154@std.stei.itb.ac.id>
2023-12-07 09:33:43 +0700

    remove controllers

--- app/controller/product.py
+++
@@ -1,9 +0,0 @@
-from .auth import get_api_data
-
-def show_all_products():
-    url = 'https://tst-api-order-production.up.railway.app/products'
-    return get_api_data(url)
-
-def show_product_by_id(product_id: int):
-    url = f'https://tst-api-order-production.up.railway.app/products/{product_id}'
-    return get_api_data(url)
